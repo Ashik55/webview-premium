034b251413242f43b2aa304563dd358c0ddc2919
ashikur <ashikur@geekpicker.com>
2023-01-22 00:58:20 +0600

    Initial Commit

---
+++ webview-premium/webview_premiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_premiumApp.swift
+//  webview-premium
+//
+//  Created by ashikur on 22/1/23.
+//
+
+import SwiftUI
+
+@main
+struct webview_premiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
